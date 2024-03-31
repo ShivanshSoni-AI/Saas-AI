727030d3cc448bf84c561923480ea6a77777c538
mohitsarawgi <mohit9632147@gmail.com>
2024-03-31 14:14:36 +0530

    clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,10 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
+
+
+   publicRoutes: ['/api/webhooks/clerk']
+
   // Allow signed out users to access the specified routes:
   // publicRoutes: ['/anyone-can-visit-this-route'],
 });
